2f231bd9f7ae32b4fab0f8c88971574fdb76acef
jlmobbex <javier.lopez@mobbex.com>
2020-09-30 10:19:41 -0300

    Save and show mobbex response data

--- Mobbex/Webpay/Block/Info.php
+++ Mobbex/Webpay/Block/Info.php
@@ -46,25 +46,33 @@ class Info extends \Magento\Payment\Block\Info
         $data = [];
 
         $info = $this->getInfo();
-
-        $paymentData = $info->getAdditionalInformation("data");
-
-        $paymentId = $info->getAdditionalInformation("id");
-        $paymentMethod = $info->getAdditionalInformation("paymentMethod");
-        $paymentStatusText = $info->getAdditionalInformation("statusText");
-
+        
+        $mobbexData   = $info->getAdditionalInformation("mobbex_data");
+        $paymentId    = $mobbexData['payment']['id'];
+        $orderUrl     = $info->getAdditionalInformation('mobbex_order_url');
+        $cardInfo     = $info->getAdditionalInformation('mobbex_card_info');
+        $cardPlan     = $info->getAdditionalInformation('mobbex_card_plan');
+        
         if (isset($paymentId) && !empty($paymentId)) {
             $data[_("Transaction ID")] = $paymentId;
         }
-
-        if (isset($paymentMethod) && !empty($paymentMethod)) {
-            $data[_("Payment Method")] = $paymentMethod;
+        
+        if (isset($orderUrl) && !empty($orderUrl)) {
+            $data[_("Order URL")] = $orderUrl;
         }
-
-        if (isset($paymentStatusText) && !empty($paymentStatusText)) {
-            $data[_("Payment Status")] = $paymentStatusText;
+        
+        if (isset($cardInfo) && !empty($cardInfo)) {
+            $data[_("Card Information")] = $cardInfo;
         }
-
+        
+        if (isset($cardPlan) && !empty($cardPlan)) {
+            $data[_("Card Plan")] = $cardPlan;
+        }
+        
+        if (isset($mobbexData['payment']['riskAnalysis']['level']) && !empty($mobbexData['payment']['riskAnalysis']['level'])) {
+            $data[_("Risk Analysis")] = $mobbexData['payment']['riskAnalysis']['level'];
+        }
+        
         return $transport->setData(array_merge($data, $transport->getData()));
     }
 }
