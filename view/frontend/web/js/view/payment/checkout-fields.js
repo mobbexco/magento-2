33dbf075393c1097aee1f2261fcc79bf36be5511
jlmobbex <javier.lopez@mobbex.com>
2021-08-03 21:26:53 -0300

    Fix address custom attributes verification

--- view/frontend/web/js/view/payment/checkout-fields.js
+++ view/frontend/web/js/view/payment/checkout-fields.js
@@ -12,25 +12,18 @@ define(
                 var shippingAddress = quote.shippingAddress();
 
                 // Exit if dni option is disabled
-                if (shippingAddress.customAttributes === undefined) {
+                if (typeof shippingAddress.customAttributes === 'undefined' || !Array.isArray(shippingAddress.customAttributes))
                     return originalAction();
-                }
 
-                if (shippingAddress['extension_attributes'] === undefined) {
+                if (typeof shippingAddress['extension_attributes'] === 'undefined')
                     shippingAddress['extension_attributes'] = {};
-                }
 
                 // Get DNI field
-                var dniField = shippingAddress.customAttributes.find(
-                    function (element) {
-                        return element.attribute_code === 'mbbx_dni';
-                    }
-                );
+                var dniField = shippingAddress.customAttributes.find(field => field.attribute_code === 'mbbx_dni');
 
                 // Add to shipping adress
-                if (dniField) {
+                if (dniField)
                     shippingAddress['extension_attributes']['mbbx_dni'] = dniField.value;
-                }
 
                 return originalAction();
             });
