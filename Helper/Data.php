0c45a0ff638174189b1c3f851487f89cdf61b167
jlmobbex <javier.lopez@mobbex.com>
2022-03-15 14:37:07 -0300

    Move executeHook method to mobbex helper

--- Helper/Data.php
+++ Helper/Data.php
@@ -71,9 +71,7 @@ class Data extends AbstractHelper
         ObjectManagerInterface $_objectManager,
         LoggerInterface $logger,
         Mobbex $mobbex,
-        \Mobbex\Webpay\Model\CustomFieldFactory $customFieldFactory,
-        \Magento\Framework\Event\ConfigInterface $eventConfig,
-        \Magento\Framework\Event\ObserverFactory $observerFactory
+        \Mobbex\Webpay\Model\CustomFieldFactory $customFieldFactory
     ) {
         $this->order = $order;
         $this->modelOrder = $modelOrder;
@@ -84,8 +82,6 @@ class Data extends AbstractHelper
         $this->_objectManager = $_objectManager;
         $this->log = $logger;
         $this->customFields = $customFieldFactory->create();
-        $this->eventConfig     = $eventConfig;
-        $this->observerFactory = $observerFactory;
     }
 
     /**
@@ -334,48 +330,5 @@ class Data extends AbstractHelper
     public function getProductSubscription($id)
     {
         return $this->mobbex->getProductSubscription($id);
-    }
-
-    /**
-     * Execute a hook and retrieve the response.
-     * 
-     * @param string $name The hook name (in camel case).
-     * @param bool $filter Filter first arg in each execution.
-     * @param mixed ...$args Arguments to pass.
-     * 
-     * @return mixed Last execution response or value filtered. Null on exceptions.
-     */
-    public function executeHook($name, $filter = false, ...$args)
-    {
-        try {
-            // Use snake case to search event
-            $eventName = ltrim(strtolower(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $name)), '_');
-
-            // Get registered observers and first arg to return as default
-            $observers = $this->eventConfig->getObservers($eventName) ?: [];
-            $value     = $filter ? reset($args) : false;
-
-            foreach ($observers as $observerData) {
-                // Instance observer
-                $instanceMethod = !empty($observerData['shared']) ? 'get' : 'create';
-                $observer       = $this->observerFactory->$instanceMethod($observerData['instance']);
-
-                // Get method to execute
-                $method = [$observer, $name];
-
-                // Only execute if is callable
-                if (!empty($observerData['disabled']) || !is_callable($method))
-                    continue;
-
-                $value = call_user_func_array($method, $args);
-
-                if ($filter)
-                    $args[0] = $value;
-            }
-
-            return $value;
-        } catch (\Exception $e) {
-            self::log('Mobbex Hook Error: ' . $e->getMessage(), 'mobbex_error.log');
-        }
     }
 }
--- Helper/Mobbex.php
+++ Helper/Mobbex.php
@@ -114,6 +114,12 @@ class Mobbex extends AbstractHelper
      */
     protected $productRepository;
 
+    /** @var \Magento\Framework\Event\ConfigInterface */ 
+    public $eventConfig;
+
+    /** @var \Magento\Framework\Event\ObserverFactory */
+    public $observerFactory;
+
     /**
      * Mobbex constructor.
      * @param Config $config
@@ -151,7 +157,9 @@ class Mobbex extends AbstractHelper
         Session $customerSession,
         EventManager $eventManager,
         SessionManagerInterface $session,
-        ProductRepository $productRepository
+        ProductRepository $productRepository,
+        \Magento\Framework\Event\ConfigInterface $eventConfig,
+        \Magento\Framework\Event\ObserverFactory $observerFactory
     ) {
         $this->config = $config;
         $this->order = $order;
@@ -171,6 +179,8 @@ class Mobbex extends AbstractHelper
         $this->eventManager = $eventManager;
         $this->session = $session;
         $this->productRepository = $productRepository;
+        $this->eventConfig     = $eventConfig;
+        $this->observerFactory = $observerFactory;
     }
 
     /**
@@ -713,22 +723,44 @@ class Mobbex extends AbstractHelper
     /**
      * Execute a hook and retrieve the response.
      * 
-     * @param string $hookName The hook name.
-     * @param array $params Associative array that will be passed as params.
+     * @param string $name The hook name (in camel case).
+     * @param bool $filter Filter first arg in each execution.
+     * @param mixed ...$args Arguments to pass.
      * 
-     * @return array
+     * @return mixed Last execution response or value filtered. Null on exceptions.
      */
-    public function executeHook($hookName, $params = null)
+    public function executeHook($name, $filter = false, ...$args)
     {
-        // Dispatch event and init session to get response
-        $this->eventManager->dispatch($hookName, $params);
-        $this->session->start()->setMobbexData($params);
+        try {
+            // Use snake case to search event
+            $eventName = ltrim(strtolower(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $name)), '_');
 
-        // Get response and clear data
-        $response = $this->session->getMobbexData();
-        $this->session->setMobbexData(null);
+            // Get registered observers and first arg to return as default
+            $observers = $this->eventConfig->getObservers($eventName) ?: [];
+            $value     = $filter ? reset($args) : false;
 
-        return $response;
+            foreach ($observers as $observerData) {
+                // Instance observer
+                $instanceMethod = !empty($observerData['shared']) ? 'get' : 'create';
+                $observer       = $this->observerFactory->$instanceMethod($observerData['instance']);
+
+                // Get method to execute
+                $method = [$observer, $name];
+
+                // Only execute if is callable
+                if (!empty($observerData['disabled']) || !is_callable($method))
+                    continue;
+
+                $value = call_user_func_array($method, $args);
+
+                if ($filter)
+                    $args[0] = $value;
+            }
+
+            return $value;
+        } catch (\Exception $e) {
+            Data::log('Mobbex Hook Error: ' . $e->getMessage(), 'mobbex_error.log');
+        }
     }
 
     /**
