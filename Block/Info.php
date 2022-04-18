2acef87a4afdc976727bc6486d69ed9788e3b448
jlmobbex <javier.lopez@mobbex.com>
2022-04-18 17:04:32 -0300

    Some improvements on order info widget

--- Block/Info.php
+++ Block/Info.php
@@ -49,30 +49,30 @@ class Info extends \Magento\Payment\Block\Info
 
         $mobbexData = $this->mobbexTransaction->getTransaction($this->getInfo()->getOrder()->getIncrementId(), [true,true]);
         $cards = [];
-        
-        if($mobbexData && $mobbexData['operation_type'] == "payment.multiple-sources")
+
+        if (isset($mobbexData['operation_type']) && $mobbexData['operation_type'] == "payment.multiple-sources")
             $cards = $this->mobbexTransaction->getTransaction($this->getInfo()->getOrder()->getIncrementId(), [true,false]);
-        
+
         $data = [
-        (string) __('Transaction ID') => isset($mobbexData['payment_id']) ? $mobbexData['payment_id'] : '',
-        (string) __('Total')          => isset($mobbexData['total']) ? $mobbexData['total'] : '',
+            (string) __('Transaction ID') => isset($mobbexData['payment_id']) ? $mobbexData['payment_id'] : '',
+            (string) __('Total')          => isset($mobbexData['total']) ? $mobbexData['total'] : '',
         ];
 
         if($cards){
             $data[(string) __('Payment Method')] = 'Multiple Cards';
             foreach ($cards as $key => $card) {
                 if($card['source_name'] !== 'multicard'){
-                    $data[(string) __('Card '.($key + 1))] = isset($card['source_name']) ? $card['source_name'] : '';
-                    $data[(string) __('Card '.($key + 1).' number')] = isset($card['source_number']) ? $card['source_number'] : '';
-                    $data[(string) __('Card '.($key + 1).' Installment')] = isset($card['source_installment']) ? json_decode($card['source_installment'], true)['description'] . ' ' . json_decode($card['source_installment'], true)['count'] . ' cuota/s' : '';
-                    $data[(string) __('Card '.($key + 1).' amount')] = isset($card['total']) ? $card['total'] : '';
+                    $data[(string) __('Card') . ' ' . ($key + 1)]                = isset($card['source_name']) ? $card['source_name'] : '';
+                    $data[(string) __('Card') . ' ' . ($key + 1).' Number']      = isset($card['source_number']) ? $card['source_number'] : '';
+                    $data[(string) __('Card') . ' ' . ($key + 1).' Installment'] = !empty($card['source_installment']) ? json_decode($card['source_installment'], true)['description'] . ' ' . json_decode($card['source_installment'], true)['count'] . ' cuota/s' : '';
+                    $data[(string) __('Card') . ' ' . ($key + 1).' Amount']      = isset($card['total']) ? $card['total'] : '';
                 }
             }
         } else {
-            $data[(string) __('Payment Method')] = isset($mobbexData['source_type']) ? $mobbexData['source_type'] : '';
-            $data[(string) __('Payment Source')] = isset($mobbexData['source_name']) ? $mobbexData['source_name'] : '';
-            $data[(string) __('Source Info')] = isset($mobbexData['source_number']) ? $mobbexData['source_number'] : '';
-            $data[(string) __('Source Installment')] = isset($mobbexData['source_installment']) ? json_decode($mobbexData['source_installment'], true)['description'] . ' ' . json_decode($mobbexData['source_installment'], true)['count'] . ' cuota/s' : '';
+            $data[(string) __('Payment Method')]     = isset($mobbexData['source_type']) ? $mobbexData['source_type'] : '';
+            $data[(string) __('Payment Source')]     = isset($mobbexData['source_name']) ? $mobbexData['source_name'] : '';
+            $data[(string) __('Source Number')]      = isset($mobbexData['source_number']) ? $mobbexData['source_number'] : '';
+            $data[(string) __('Source Installment')] = !empty($mobbexData['source_installment']) ? json_decode($mobbexData['source_installment'], true)['description'] . ' ' . json_decode($mobbexData['source_installment'], true)['count'] . ' cuota/s' : '';
         }
 
         // Only show in admin panel
@@ -80,8 +80,8 @@ class Info extends \Magento\Payment\Block\Info
             $coupon = isset($mobbexData['entity_uid']) && isset($mobbexData['payment_id']) ? "https://mobbex.com/console/" . $mobbexData['entity_uid'] . "/operations/?oid=" . $mobbexData['payment_id'] : '';
 
             $data = array_merge($data, [
-                (string) __('Risk Analisys') => isset($mobbexData['risk_analysis']) ? $mobbexData['risk_analysis'] : '',
-                (string) __('Coupon') => isset($mobbexData['payment_id']) && isset($mobbexData['entity_uid']) ? $coupon : '',
+                (string) __('Risk Analysis') => isset($mobbexData['risk_analysis']) ? $mobbexData['risk_analysis'] : '',
+                (string) __('Coupon')        => isset($mobbexData['payment_id']) && isset($mobbexData['entity_uid']) ? $coupon : '',
             ]);
         }
 
