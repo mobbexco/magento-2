26e5435f877c9a07cacc44f426878ca0e653709d
jlmobbex <javier.lopez@mobbex.com>
2022-05-13 18:58:39 -0300

    Update version

--- Helper/Mobbex.php
+++ Helper/Mobbex.php
@@ -27,7 +27,7 @@ use Magento\Catalog\Model\ProductRepository;
  */
 class Mobbex extends AbstractHelper
 {
-    const VERSION = '3.0.1';
+    const VERSION = '3.0.2';
 
     /**
      * @var Config
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-ver="3.0.1"
+ver="3.0.2"
 
 # Compress archive
 7z a -tzip mobbex.$ver.mag-2.zip * -xr!.git -xr!.vscode -x!*.zip -x!build.sh -x!readme.md -x!.gitignore
