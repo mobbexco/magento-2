6b7e5b43dd4fa675a28a7af27e8cc41ab1786aa6
jlmobbex <javier.lopez@mobbex.com>
2022-11-14 14:10:39 -0300

    Add capture button to order admin panel

---
+++ Controller/AdminHtml/Payment/Capture.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Mobbex\Webpay\Controller\AdminHtml\Payment;
+
+class Capture extends \Magento\Backend\App\Action
+{
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Mobbex\Webpay\Helper\Instantiator $instantiator
+    ) {
+        parent::__construct($context);
+        $instantiator->setProperties($this, ['sdk', 'logger', 'mobbexTransactionFactory', '_order']);
+    }
+
+    public function execute()
+    {
+        $resultRedirect = $this->resultRedirectFactory->create();
+
+        try {
+            // Get order with him id
+            $id    = $this->getRequest()->getParam('order_id');
+            $order = $this->_order->load($id);
+
+            // Get transaction data from db
+            $transaction = $this->mobbexTransactionFactory->create()->getTransaction($order->getIncrementId(), [1, 1]);
+
+            // Make capture request
+            $result = \Mobbex\Api::request([
+                'method' => 'POST',
+                'uri'    => "operations/$transaction[payment_id]/capture",
+                'body'   => ['total' => $order->getGrandTotal()],
+            ]);
+
+            if (!$result)
+                throw new \Exception('Uncaught Exception on Mobbex Request', 500);
+
+            $this->messageManager->addSuccessMessage('Operación capturada correctamente');
+        } catch (\Exception $e) {
+            // Add message to admin panel and debug
+            $this->messageManager->addErrorMessage($e->getMessage());
+            $this->logger->debug('err', $e->getMessage(), isset($e->data) ? $e->data : []);
+        }
+
+        return $resultRedirect->setPath('sales/order/view', ['order_id' => $id]);
+    }
+}
