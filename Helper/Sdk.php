960606becbedd8871afd08e5077d85d2edfe1e56
jlmobbex <javier.lopez@mobbex.com>
2022-11-16 15:51:13 -0300

    Minor fix obtaining module version

--- Helper/Sdk.php
+++ Helper/Sdk.php
@@ -29,11 +29,10 @@ class Sdk extends \Magento\Framework\App\Helper\AbstractHelper
     public function init()
     {
         // Set platform information
-        \Mobbex\Platform::init('magento_2', $this->moduleResource->getDbVersion('Mobbex'), $this->_urlBuilder->getUrl('/'),
+        \Mobbex\Platform::init('magento_2', $this->moduleResource->getDbVersion('Mobbex_Webpay'), $this->_urlBuilder->getUrl('/'),
         [
-            'magento' => $this->productMetadata->getVersion(),
-            'webpay'  => $this->moduleResource->getDbVersion('Mobbex_Webpay'),
-            'sdk'     => class_exists('\Composer\InstalledVersions') ? \Composer\InstalledVersions::getVersion('mobbexco/php-plugins-sdk') : '',
+            'platform' => $this->productMetadata->getVersion(),
+            'sdk'      => class_exists('\Composer\InstalledVersions') ? \Composer\InstalledVersions::getVersion('mobbexco/php-plugins-sdk') : '',
         ], $this->config->getAll(), [$this->helper, 'executeHook']);
 
         // Init api conector
