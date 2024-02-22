ba5e5979d85c381e62c12da618d0bfbdc13d5dc1
EMunozMobbex <e.munoz.chedel@mobbex.com>
2024-02-22 14:59:13 -0300

    Update version.

--- Helper/Mobbex.php
+++ Helper/Mobbex.php
@@ -8,7 +8,7 @@ namespace Mobbex\Webpay\Helper;
  */
 class Mobbex extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    const VERSION = '3.10.0';
+    const VERSION = '3.10.1';
 
     /** @var ScopeConfigInterface */
     public $scopeConfig;
