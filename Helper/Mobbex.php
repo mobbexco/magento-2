78f80c99abdb0b3b47811c70a240945f3d4cb2e8
jlmobbex <javier.lopez@mobbex.com>
2024-07-02 19:25:28 -0300

    Update version

--- Helper/Mobbex.php
+++ Helper/Mobbex.php
@@ -8,7 +8,7 @@ namespace Mobbex\Webpay\Helper;
  */
 class Mobbex extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    const VERSION = '3.13.0';
+    const VERSION = '3.14.0';
 
     /** @var ScopeConfigInterface */
     public $scopeConfig;
--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ver="3.13.0"
+ver="3.14.0"
 
 # Remove installed packages
 rm -rf vendor composer.lock
