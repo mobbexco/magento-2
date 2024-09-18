134e7c95d4f93589276f2f16165125d9af40f294
jlmobbex <javier.lopez@mobbex.com>
2024-09-18 16:50:17 -0300

    Update version

--- Helper/Mobbex.php
+++ Helper/Mobbex.php
@@ -8,7 +8,7 @@ namespace Mobbex\Webpay\Helper;
  */
 class Mobbex extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    const VERSION = '3.15.0';
+    const VERSION = '4.0.0';
 
     /** @var ScopeConfigInterface */
     public $scopeConfig;
