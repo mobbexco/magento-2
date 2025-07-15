414ee6d129617c58e2747f92324c4d9cdb7a30d3
mobbexLuis <luis.scarrone@mobbex.com>
2025-07-15 15:53:18 -0300

    *new controller: Sources It manages and procces data to be send to Mobbex API

---
+++ Controller/Payment/Sources.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Mobbex\Webpay\Controller\Payment;
+
+/**
+ * Class Checkout
+ * @package Mobbex\Webpay\Controller\Frontend\FinanceWidget;
+ */
+
+class Sources implements \Magento\Framework\App\Action\HttpGetActionInterface
+{
+    /** @var \Mobbex\Webpay\Helper\Sdk */
+    public $sdk;
+
+    /** @var \Mobbex\Webpay\Helper\Mobbex */
+    public $helper;
+
+    /** @var \Mobbex\Webpay\Helper\Logger */
+    public $logger;
+
+    /** @var \Mobbex\Webpay\Helper\Config */
+    public $config;
+
+    /** @var \Magento\Framework\App\RequestInterface $request, */
+    public $_request;
+
+    /** @var \Magento\Catalog\Model\ProductFactory */
+    public $_productFactory;
+
+    public function __construct(
+        \Mobbex\Webpay\Helper\Sdk $sdk,
+        \Mobbex\Webpay\Helper\Mobbex $helper,
+        \Mobbex\Webpay\Helper\Logger $logger,
+        \Mobbex\Webpay\Helper\Config $config,
+        \Magento\Framework\App\RequestInterface $request,
+        \Magento\Catalog\Model\ProductFactory $productFactory
+        
+    )
+    {
+        $this->sdk             = $sdk;
+        $this->helper          = $helper;
+        $this->logger          = $logger;
+        $this->config          = $config;
+        $this->_request        = $request;
+        $this->_productFactory = $productFactory;
+
+        //Init mobbex php plugins sdk
+        $this->sdk->init();
+    }
+
+    /**
+     * Execute method to handle the request and return sources from Mobbex API
+     *
+     * @return ResponseInterface|ResultInterface
+     */
+    public function execute()
+    {
+        // Gets query params
+        $total       = $this->_request->getParam('mbbxTotal');
+        $product_ids = $this->_request->getParam('mbbxProductIds');
+
+        // Filters out non-numeric values
+        $product_ids = array_filter($product_ids, function ($id) {
+            return is_numeric($id);
+        });
+
+        // Gets products objects
+        if (count($product_ids) > 1)
+            $products= array_map(function ($product_id) {
+                return $this->_productFactory->create()->load($product_id);
+                }, $product_ids);
+        else
+            $products = $this->_productFactory->create()->load($product_ids[0]);
+
+        // Extracts products plans
+        extract($this->config->getProductPlans($products));
+
+        $installments = \Mobbex\Repository::getInstallments(
+                [$products],
+                $common_plans,
+                $advanced_plans
+            );
+
+        try {
+            $sources = \Mobbex\Repository::getSources(
+                $total,
+                $installments
+            );
+    
+            die (json_encode(
+                [
+                    'success' => true,
+                    'sources' => $sources,
+                ]
+            ));
+
+        } catch (\Exception $e) {
+            $this->logger->log(
+                'error', 'Error getting sources', 
+                [
+                    'message' => $e->getMessage(),
+                    'code'    => $e->getCode(),
+                ]
+            );
+
+            die (json_encode(
+                [
+                    'success' => false,
+                    'message' => $e->getMessage(),
+                ]
+            ));
+        }
+    }
+}
