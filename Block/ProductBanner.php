55be984a70293f6f61d9ebb1f7c5ff91625465af
mobbexLuis <luis.scarrone@mobbex.com>
2025-11-18 11:53:12 -0300

    added template and block to interact with product-tags script in required views

---
+++ Block/ProductBanner.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Mobbex\Webpay\Block;
+
+class ProductBanner extends \Magento\Framework\View\Element\Template
+{
+    /** @var \Mobbex\Webpay\Helper\Config */
+    public $config;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Mobbex\Webpay\Helper\Config $config,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->config = $config;
+    }
+
+    /**
+     * Checks if the product catalog tag feature is enabled.
+     *
+     * @return bool
+     */
+    public function isTagEnabled()
+    {
+        return (bool) $this->config->get('show_tag_on_products_catalog');
+    }
+
+    /**
+     * Checks if the product catalog banner feature is enabled.
+     *
+     * @return bool
+     */
+    public function isBannerEnabled()
+    {
+        return (bool) $this->config->get('show_banner_on_products_catalog');
+    }
+
+    /**
+     * Returns controller URL to get finance data.
+     *
+     * @return string
+     */
+    public function getFinanceDataUrl()
+    {
+        return $this->getUrl('sugapay/catalog/financedata');
+    }
+}
