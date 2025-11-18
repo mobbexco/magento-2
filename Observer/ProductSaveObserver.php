116f7daa9360c4cff182378886d5cbee3912e110
mobbexLuis <luis.scarrone@mobbex.com>
2025-11-18 11:53:54 -0300

    added backend logic to save and get bestPlan finance data

--- Observer/ProductSaveObserver.php
+++ Observer/ProductSaveObserver.php
@@ -10,6 +10,15 @@ class ProductSaveObserver implements ObserverInterface
     /** @var \Mobbex\Webpay\Helper\Mobbex */
     public $helper;
 
+    /** @var \Mobbex\Webpay\Helper\Config */
+    public $config;
+    
+    /** @var \Mobbex\Webpay\Helper\Logger */
+    public $logger;
+
+    /** @var \Mobbex\Webpay\Helper\Sdk */
+    public $sdk;
+
     /** @var \Mobbex\Webpay\Model\CustomFieldFactory */
     public $customFieldFactory;
 
@@ -22,14 +31,21 @@ class ProductSaveObserver implements ObserverInterface
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Mobbex\Webpay\Helper\Mobbex $helper,
+        \Mobbex\Webpay\Helper\Config $config,
         \Mobbex\Webpay\Model\CustomFieldFactory $customFieldFactory,
-        \Magento\Framework\Serialize\Serializer\Serialize $serialize
+        \Magento\Framework\Serialize\Serializer\Serialize $serialize,
+        \Mobbex\Webpay\Helper\Logger $logger,
+        \Mobbex\Webpay\Helper\Sdk $sdk
     )
     {
         $this->helper             = $helper;
+        $this->config             = $config;
+        $this->logger             = $logger;
         $this->customFieldFactory = $customFieldFactory;
         $this->params             = $context->getRequest()->getParams();
         $this->serializer         = $serialize;
+        $this->sdk                = $sdk;
+        $this->sdk->init();
     }
 
     /**
@@ -54,13 +70,86 @@ class ProductSaveObserver implements ObserverInterface
             'selected_plans'   => isset($this->params['mobbex_selected_plans']) ? $this->params['mobbex_selected_plans'] : "[]",
             'show_featured'    => isset($this->params['mobbex_show_featured_plans']) ? $this->params['mobbex_show_featured_plans'] : "no",
         ];
+
+        $product = $observer->getProduct();
+        $id      = $product->getId();
         
         //Save mobbex custom fields
         foreach ($productConfigs as $key => $value) {
             $customField = $this->customFieldFactory->create();
-            $customField->saveCustomField($observer->getProduct()->getId(), 'product', $key, $value);
+            $customField->saveCustomField($id, 'product', $key, $value);
         }
 
-        $this->helper->executeHook('mobbexSaveProductSettings', false, $observer->getProduct(), $this->params);
+        $this->saveBestPlan($product);
+        $this->helper->executeHook('mobbexSaveProductSettings', false, $product, $this->params);
+    }
+
+    /**
+     * saveBestPlan saves the required data to show the best plan banner in products catalog page
+     * 
+     * @param object $product
+     */
+    private function saveBestPlan($product = null)
+    {
+        if (!$product)
+            return null;
+
+        $id = $product->getId();
+
+        $featuredPlans = $this->config->getAllPlansConfiguratorSettings($product, "manual_config")
+            ? json_decode($this->config->getAllPlansConfiguratorSettings($product, "featured_plans"), true)
+            : null;
+
+        if (empty($featuredPlans))
+            return null;
+
+        $best_plan = $this->getBestPlan($featuredPlans, $product, $id);
+
+        $customField = $this->customFieldFactory->create();
+        $customField->saveCustomField($id, 'product', 'bestPlan', $best_plan);
+    }
+
+    /**
+     * getBestPlan get the best plan configured as featured plan for a product
+     * 
+     * @param array      $featuredPlans
+     * @param int|string $id
+     * 
+     * @return null|string best plan in featured plans
+     */
+    private function getBestPlan($featuredPlans, $product, &$id) 
+    {
+        $sources = [];
+        // $product = $observer->getProduct();
+        $total   = $product->getPriceInfo()->getPrice('final_price')->getValue();
+
+        // Get product plans
+        extract($this->config->getProductPlans($product));
+
+        $installments = \Mobbex\Repository::getInstallments(
+            [$id],
+            $common_plans,
+            $advanced_plans
+        );
+
+        // Get sources from cache or Mobbex API
+        try {
+            $sources = \Mobbex\Repository::getSources(
+                $total,
+                $installments
+            );
+        }  catch (\Exception $e) {
+            $this->logger->log(
+                'error', 
+                'ProductSaveObserver > getSources', 
+                $e->getMessage()
+            );
+            return null;
+        }
+        
+        if (empty($sources))
+            return null;
+
+        return $this->helper->filterFeaturedPlans($sources, $featuredPlans);
     }
 }
